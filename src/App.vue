00d1f3eb6d6a485f0d7b6724094609ed3cf24b4c
Shii108 <sujalpoudel01@gmail.com>
2025-04-23 10:42:51 +0545

    removed unwanted css properties

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 <script setup>
 import { onMounted } from 'vue';  
 import {useNewsStore} from "../src/store/newsStore.js";
-import layout from '../src/components/Layout.vue';
 
 const newsStore = useNewsStore();
 onMounted(() => {
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,13 +1,15 @@
 <template>
 <header>
   <h1 class="text-2xl text-red">News Portal</h1>
-  <nav>
+  <h1>this is just another test</h1>
+  <h1 class="border">this is also another testing</h1>
+  <!-- <nav>
     <ul>
       <li>Home</li>
       <li>Testing</li>
       <li>Contact Me</li>
     </ul>
-  </nav>
+  </nav> -->
 </header>
 </template>
 
